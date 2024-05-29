7ce65aab44d8a8b4551f857ef5d5a5addfd9f626
Sergey Chv <serge.techno@gmail.com>
2024-05-29 09:38:38 +0300

    finished Single Page Design

--- src/components/CustomizeProducts.tsx
+++ src/components/CustomizeProducts.tsx
@@ -16,7 +16,11 @@ const CustomizeProducts = () => {
             </li>
         </ul>
         <h4 className="font-medium">Choose a size</h4>
-        <ul className="flex items-center gap-3"></ul>
+        <ul className="flex items-center gap-3">
+            <li className="ring-1 ring-cartItem text-cartItem rounded-md px-4 py-1 text-sm cursor-pointer">Small</li>
+            <li className="ring-1 ring-cartItem text-white bg-cartItem rounded-md px-4 py-1 text-sm cursor-pointer">Medium</li>
+            <li className="ring-1 ring-pink-200 text-white bg-pink-200 rounded-md px-4 py-1 text-sm cursor-pointer">Large</li>
+        </ul>
       </div>
     )
   }
