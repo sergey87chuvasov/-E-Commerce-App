af65659c9280b572b793a60652068d6da4181b96
Sergey87 <serge.techno@gmail.com>
2024-06-17 21:43:04 +0300

    work with Wix Headless

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,15 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
-import Navbar from "@/components/Navbar";
-import Footer from "@/components/Footer";
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import './globals.css';
+import Navbar from '@/components/Navbar';
+import Footer from '@/components/Footer';
+import { WixClientContextProvider } from '@/context/wixContext';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: "E-Commerce Application",
-  description: "A complete e-commerce application with Next.js and Wix",
+  title: 'E-Commerce Application',
+  description: 'A complete e-commerce application with Next.js and Wix',
 };
 
 export default function RootLayout({
@@ -17,12 +18,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang='en'>
       <body className={inter.className}>
-        <Navbar />
-         {children}
-        <Footer />
-        </body>
+        <WixClientContextProvider>
+          <Navbar />
+          {children}
+          <Footer />
+        </WixClientContextProvider>
+      </body>
     </html>
   );
 }
